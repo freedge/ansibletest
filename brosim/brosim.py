11af4f599dd3a32d87f688be22ff3278657443c2
Francois RIGAULT <rigault.francois@gmail.com>
2020-07-31 22:26:57 +0200

    Move to logging

--- brosim/brosim.py
+++ brosim/brosim.py
@@ -5,6 +5,7 @@ from fake_switches.netconf import dict_2_etree
 from lxml import etree
 import time
 import json
+import logging
 
 CONNECTION_ID=0
  
@@ -12,14 +13,14 @@ class Brocade(resource.Resource):
     isLeaf = True
     def render_PATCH(self, request):
         content = request.content.read().decode("utf-8")
-        print("PATCH", request.getHeader('authorization'), request.uri, content)
+        logging.info("PATCH %s %r %s", request.getHeader('authorization'), request.uri, content)
         res=dict(Response={
         })
         return etree.tostring(dict_2_etree(res), pretty_print=True)
 
     def render_GET(self, request):
         content = request.content.read().decode("utf-8")
-        print("GET", request.getHeader('authorization'), request.uri, content)
+        logging.info("GET %s %r %s", request.getHeader('authorization'), request.uri, content)
         if request.uri == b"/rest/running/brocade-fibrechannel-switch/fibrechannel-switch":
             res=dict(Response={
                 "fibrechannel-switch":{
@@ -63,11 +64,11 @@ class Brocade(resource.Resource):
         content = request.content.read().decode("utf-8")
         if request.uri == b"/rest/login":
             time.sleep(0.1) # this can take time
-            print("POST", "login", request.uri, content)
+            logging.info("POST login %r %s", request.uri, content)
             CONNECTION_ID += 1
             request.responseHeaders.addRawHeader("Authorization", "CNX" + str(CONNECTION_ID))
         else:
-            print("POST", request.getHeader('authorization'), request.uri, content)
+            logging.info("POST %r %r %s", request.getHeader('authorization'), request.uri, content)
         return b"<html>Hello, world!</html>"
 
 
@@ -78,23 +79,25 @@ class Svc(resource.Resource):
         content = request.content.read().decode("utf-8")
         if request.uri == b"/rest/auth":
             time.sleep(0.1) # this can take time
-            print("POST", "auth", request.uri, content)
+            logging.info("POST auth %r %s", request.uri, content)
             CONNECTION_ID += 1
             b = json.dumps({"token": "CNX" + str(CONNECTION_ID)})
-            print("return:", b)
             return b.encode('utf-8')
         elif request.uri == b'/rest/lshost/test1':
-            print("POST", request.getHeader('x-auth-token'), request.uri, content)
+            logging.info("POST %s %r %s", request.getHeader('x-auth-token'), request.uri, content)
             res = {'id': '226', 'name': 'test1', 'port_count': '2', 'type': 'generic', 'mask': '1111111111111111111111111111111111111111111111111111111111111111', 'iogrp_count': '1', 'status': 'offline', 'site_id': '', 'site_name': '', 'host_cluster_id': '', 'host_cluster_name': '', 'protocol': 'scsi', 'nodes': [{'WWPN': 'abc', 'node_logged_in_count': '0', 'state': 'offline'}, {'WWPN': 'def', 'node_logged_in_count': '0', 'state': 'offline'}]}
             return json.dumps(res).encode('utf-8')
 
         elif request.uri == b'/rest/mkhost': 
-            print("POST", request.getHeader('x-auth-token'), request.uri, content)
+            logging.info("POST %s %r %s", request.getHeader('x-auth-token'), request.uri, content)
             return json.dumps({"message": "ca roule"}).encode('utf-8')
         else:
-            print("POST", request.getHeader('x-auth-token'), request.uri, content)
+            logging.info("POST %s %r %s", request.getHeader('x-auth-token'), request.uri, content)
         return b"<html>Hello, world!</html>"
 
+logging.basicConfig(level='DEBUG')
+logger = logging.getLogger()
+logger.setLevel('DEBUG')
 
 site = server.Site(Brocade())
 endpoint = endpoints.SSL4ServerEndpoint(reactor, 8443, ssl.DefaultOpenSSLContextFactory("pkey", "cert.pem"))
